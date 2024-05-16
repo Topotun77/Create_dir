79b77191e3d17de1611ef45c79dea23ee09bc6cb
Svetlana <bel-svetik@yandex.ru>
2024-05-16 12:57:44 +0300

    Добавить исполняемый файл создания директорий

--- Create_dir.py
+++ Create_dir.py
@@ -2,7 +2,7 @@ import os
 import tkinter as tk
 from tkinter import filedialog as fd
 
-print(help(tk))
+# print(help(tk))
 
 
 def ToStr(c):
@@ -44,24 +44,31 @@ window.configure(bg='orange')
 window.title('Создание директориев по списку')
 window.geometry("500x200")
 window.resizable(False, False)
+
 button_create = tk.Button(window, text="Создать директории", width=55, height=2, command=create_dir)
 button_create.place(x=35, y=100)
+
 file_entry = tk.Entry(window, width=50)
 file_entry.place(x=140, y=50)
 txt = 'list.txt'
 file_entry.insert(0, txt)
+
 dir_entry = tk.Entry(window, width=50)
 dir_entry.place(x=140, y=25)
-txt = './Verstka'
+txt = './'
+# txt = './Verstka'
 dir_entry.insert(0, txt)
+
 button_dir = tk.Button(window, text="/", width=1, height=0, command=choice_dir)
 button_dir.place(x=450, y=22)
+
 number1 = tk.Label(window, text="Файл со списком: ")
 number1.place(x=22, y=50)
 number2 = tk.Label(window, text="Директория вывода: ")
 number2.place(x=10, y=25)
+
 window.mainloop()
 
 
 # вывести текущую директорию
-print("Текущая деректория:", os.getcwd())
+# print("Текущая деректория:", os.getcwd())
